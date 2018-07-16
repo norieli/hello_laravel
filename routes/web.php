5229393c0a2d8878b427183301a242b99731814e
norie-virtual <norieli@qq.com>
2018-07-16 07:13:25 +0000

    finish user signup

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 Route::get('signup', 'UserController@create')->name('signup');
 
+Route::resource('users', 'UserController');
+
