b93054b92f212c7b21783f3ab4d037c0cdbe4bd4
vagrant <vagrant@homestead>
2018-10-17 09:29:56 +0000

    add email auth

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,5 @@ Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 Route::get('/users/{user}/edit', 'UserController@edit')->name('users.edit');
+
+Route::get('signup/confirm/{token}', 'UserController@confirmEmail')->name('confirm_email');
